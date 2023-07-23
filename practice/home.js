96cf8fc5e69e34a4cc0b6c07c6008a9a9272beca
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-07-23 16:10:20 +0530

    promises

---
+++ practice/home.js
@@ -0,0 +1,23 @@
+
+let but=document.querySelector("#button")
+let el=document.getElementById("numbertaker")
+
+but.addEventListener("click",()=>{
+    
+    let val=el.value
+    console.log(val)
+    el.value=""
+
+    const prom=new Promise((res,rej)=>{
+        if(val%2==0){
+            res(val)
+        }
+        else{
+            let err=new Error("Number is Odd")
+            rej(err)
+        }
+    })
+
+   console.log(prom)
+})
+
