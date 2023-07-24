988f1547cc64427dc35d5b0c31f2d50e3d63300c
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-07-24 11:44:46 +0530

    context api revisit

--- todo/src/Components/Todoinput.jsx
+++ todo/src/Components/Todoinput.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Todoinput = () => {
-  return (
-    <div>
-      <input placeholder='Enter Task'/>
-      <button>Add Todo</button>
-    </div>
-  )
+const Todoinput = ({status}) => {
+    const [text, setText] = useState("")
+   
+    let todos = JSON.parse(localStorage.getItem("todos")) || []
+
+    const handleAddTodo = () => {
+        const todo = {
+            task: text,
+            status: status
+        }
+        setText("")
+        todos.push(todo)
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }
+    return (
+        <div>
+            <input value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter Task' />
+            <button onClick={handleAddTodo}>Add Todo</button>
+        </div>
+    )
 }
 
 export default Todoinput
--- todo/src/Pages/Todo.jsx
+++ todo/src/Pages/Todo.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Todoinput from '../Components/Todoinput'
 
 const Todo = () => {
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <Todoinput/>
-    </div>
-  )
+    const [status, setStatus] = useState(false)
+    const todos = JSON.parse(localStorage.getItem("todos")) || []
+
+    const handleStatus = (el) => {
+        setStatus(!el.status)
+        // console.log("working")
+        console.log(el)
+        // console.log(todos)
+    }
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <Todoinput status={status} />
+            <div>
+                {todos.map(el => {
+                    return <div style={{ display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
+                        <h1>{el.task}</h1>
+                        <h1>{el.Status ? "Completed" : "Pending"}</h1>
+                        <button onClick={() => { handleStatus(el) }} style={{ width: "10%", height: "15%" }}>{el.Status ? "Mark as Pending" : "Mark as Completed"}</button>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Todo
