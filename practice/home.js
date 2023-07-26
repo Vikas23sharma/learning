41c1100e040ed8e850ab300dc3c0d844d6cd1fed
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-07-26 16:46:22 +0530

    async await

--- practice/home.js
+++ practice/home.js
@@ -1,23 +1,46 @@
 
-let but=document.querySelector("#button")
-let el=document.getElementById("numbertaker")
+let but = document.querySelector("#button")
+let el = document.getElementById("numbertaker")
 
-but.addEventListener("click",()=>{
-    
-    let val=el.value
+but.addEventListener("click", () => {
+
+    let val = el.value
     console.log(val)
-    el.value=""
+    el.value = ""
 
-    const prom=new Promise((res,rej)=>{
-        if(val%2==0){
+    const prom = new Promise((res, rej) => {
+        if (val % 2 == 0) {
             res("Number is Even Stevens")
         }
-        else{
-            let err=new Error("Number is Odd")
+        else {
+            let err = new Error("Number is Odd")
             rej(err)
         }
     })
 
-   console.log(prom)
+    console.log(prom)
 })
+
+
+let url = 'https://fakestoreapi.com/products'
+
+function getData() {
+    return fetch(url)
+        .then(res => res.json())
+        .catch((err) => console.log(err))
+}
+
+async function Data() {
+    try {
+        let data = await getData()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Data()
+
+
+
 
