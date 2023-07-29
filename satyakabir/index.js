88976847220539807b711701b164f3c53b97f5ec
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-07-30 00:14:28 +0530

    backdrop done

--- satyakabir/index.js
+++ satyakabir/index.js
@@ -8,11 +8,19 @@ function myFunction() {
     }
 }
 
-const bike=document.getElementById("jawa")
-const btn=document.getElementById("btn")
-const cont=document.getElementById("cont")
+const bike = document.getElementById("jawa")
+const btn = document.getElementById("btn")
 
-btn.addEventListener("click",function(){
-     cont.setAttribute("border","1px solid red")
-     console.log(1)
+
+btn.addEventListener("click", function () {
+    const cont = document.getElementById("cont")
+    const dialog=document.querySelector('dialog')
+    dialog.showModal()
+    const closebtn = document.getElementById("close")
+    cont.setAttribute("style", "display:block")
+    closebtn.addEventListener("click",function(){
+        dialog.close()
+        cont.setAttribute("style", "display:none")
+    })
+    console.log(1)
 })
